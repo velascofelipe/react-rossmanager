090b9023ebb04be4775820198a9b93f0b6bc4a60
Felipe Velasco <smyxone@gmail.com>
2024-01-20 03:31:55 -0300

    Actualización componentes form

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -1,56 +1,103 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const TaskForm = ({ title, description, completed, setTitle, setDescription, setCompleted, addTask }) => (
-    <form>
-        <div className="field">
-            <label className="label">Título:</label>
-            <div className="control">
-                <input
-                    type="text"
-                    className="input"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-            </div>
-        </div>
+const TaskForm = ({ title, description, completed, setTitle, setDescription, setCompleted, addTask }) => {
+    const handleAddTask = () => {
+        // Verificar que los campos no estén en blanco antes de agregar la tarea
+        if (title.trim() === '' || description.trim() === '') {
+            toast.error('Por favor, complete todos los campos.', {
+                position: 'top-right',
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+            });
+            return;
+        }
 
-        <div className="field">
-            <label className="label">Descripción:</label>
-            <div className="control">
-                <textarea
-                    className="textarea"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                ></textarea>
-            </div>
-        </div>
+        // Si los campos no están en blanco, llama a la función addTask
+        addTask();
+        // También puedes mostrar un mensaje de éxito si lo deseas
+        toast.success('Tarea agregada exitosamente', {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        });
+    };
 
-        <div className="field">
-            <div className="control">
-                <label className="checkbox">
-                    <input
-                        type="checkbox"
-                        checked={completed}
-                        onChange={(e) => setCompleted(e.target.checked)}
-                        className="mr-2" // Espaciado a la derecha
+    return (
+        <form>
+            <div className="columns">
+                <div className="column is-half">
+                    <div className="field">
+                        <label className="label">Título:</label>
+                        <div className="control">
+                            <input
+                                type="text"
+                                className="input"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Descripción:</label>
+                        <div className="control">
+                            <textarea
+                                className="textarea"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            ></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="column is-half">
+                    {/* Agrega aquí tu imagen o cualquier otro contenido que desees */}
+                    <img
+                        src="https://i.imgur.com/AL9zldG.jpg"
+                        height={200}
+                        width={250}
+                        alt="Imagen"
+                        className="image is-rounded"  // Clases de Bulma para bordes redondeados
+                        style={{ marginBottom: '10px', marginLeft: '120px', borderRadius: '30px' }}  // Estilo personalizado para espacio en la parte inferior y margen izquierdo
                     />
-                    <span className="is-size-5 has-text-weight-semibold">Completada</span>
-                </label>
+                </div>
             </div>
-        </div>
 
-        <div className="field">
-            <div className="control">
-                <button
-                    type="button"
-                    onClick={addTask}
-                    className="button is-primary"
-                >
-                    Agregar tarea
-                </button>
+            <div className="field">
+                <div className="control">
+                    <label className="checkbox">
+                        <input
+                            type="checkbox"
+                            checked={completed}
+                            onChange={(e) => setCompleted(e.target.checked)}
+                            className="mr-2"
+                        />
+                        <span className="is-size-5 has-text-weight-semibold">Completada</span>
+                    </label>
+                </div>
             </div>
-        </div>
-    </form>
-);
+
+            <div className="field">
+                <div className="control">
+                    <button
+                        type="button"
+                        onClick={handleAddTask}
+                        className="button is-primary"
+                    >
+                        Agregar tarea
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
+};
 
 export default TaskForm;
